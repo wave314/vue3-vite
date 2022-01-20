c893df4adc0b44b897b4ec334525d6e59ae8464a
wave.wei <wave.wei@shijigroup.com>
2022-01-20 20:38:23 +0800

    fix:测试 lint-staged

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ test()
   </h1>
 
   <p>
-    Recommended IDE setup:
+    Recommended IDE setup: Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
