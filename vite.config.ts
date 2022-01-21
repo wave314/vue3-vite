c3f3f16d8142e322ae951d68230e994a27b4e4ba
wave.wei <wave.wei@shijigroup.com>
2022-01-21 10:23:36 +0800

    feat:修改husky 目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const { resolve } = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wave/',
+  build: {
+    outDir: '/dist/wave'
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   server: {
     // 开发服务器配置
-    port: 8888, // 端口默认3000
-  },
+    port: 8888 // 端口默认3000
+  }
 })
